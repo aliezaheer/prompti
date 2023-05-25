7fd0f5cd58e652b749fecd667189d16d1687bc36
aliezaheer <alizaheer67@gmail.com>
2023-05-25 13:51:42 +0500

    api fix

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,36 +1,7 @@
 "use client";
 
-import { useState } from "react";
-
 const Feed = () => {
-  const [num, setNum] = useState(0);
-
-  const changeHandler = () => {
-    setNum(num + 2);
-  };
-
-  const changeHandlerClear = () => {
-    setNum(0);
-  };
-
-  return (
-    <div>
-      <h2>Api ChatGPT</h2>
-      <p>{num}</p>
-
-      <button className="bg-orange-500 p-3 rounded-md" onClick={changeHandler}>
-        Testing API
-      </button>
-      <br />
-      <br />
-      <button
-        className="bg-orange-500 p-3 rounded-md"
-        onClick={changeHandlerClear}
-      >
-        Clear State
-      </button>
-    </div>
-  );
+  return <div>Feed</div>;
 };
 
 export default Feed;
